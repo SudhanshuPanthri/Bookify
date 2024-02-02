f2940b3aa2ef56268a492f560b43415613ee19f4
sudhanshu panthri <panthrisudhanshu666@gmail.com>
2024-02-03 00:53:32 +0530

    Create data in MVC

---
+++ Migrations/20240202160900_dbInit.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookifyWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class dbInit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookifyWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+
+        [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage="Range should be more than 0 and less than 100")]
+        public int DisplayOrder { get; set; }
+    }
+}
